e0528d19b52459fe6af28cbb7682d25829e10b6d
时启龙 <shiqilong@beyondcent.com>
2022-09-26 20:44:48 +0800

    :wrench: 增加 es 模式输出

--- rollup.config.js
+++ rollup.config.js
@@ -4,6 +4,12 @@ export default {
     {
       format: 'cjs',
       file: './packages/vue/dist/mini-vue.cjs.js'
+    },
+    {
+      name: 'vue',
+      format: 'es',
+      file: './packages/vue/dist/mini-vue.esm-bundler.js',
+      sourcemap: true
     }
   ]
 }
--- scripts/rollup.config.port.js
+++
@@ -1,7 +0,0 @@
-export default {
-  input: 'src/index.js',
-  output: {
-    file: 'dist/mini-vue.js',
-    format: 'cjs',
-  },
-}
